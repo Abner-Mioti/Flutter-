42cc5df16bde6b94f99a782259bc6b6659fc6d3e
Abner-Mioti <awmmanha@hotmail.com>
2022-05-12 08:49:19 -0300

    Configuração de autenticação do FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:app07_loja_cafe/firebase_options.dart';
+import 'package:app07_loja_cafe/pages/criar_conta_page.dart';
+import 'package:app07_loja_cafe/pages/login_page.dart';
+import 'package:app07_loja_cafe/pages/principal_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
- 
-  );
-  
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Café Store',
+    initialRoute: 'login',
+    routes: {
+      'login': (context) => const LoginPage(),
+      'criar_conta': (context) => CriarContaPage(),
+      'principal': (context) => const PrincipalPage(),
+    },
+  ));
 }
---
+++ lib/pages/principal_page.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class PrincipalPage extends StatefulWidget {
+  const PrincipalPage({Key? key}) : super(key: key);
+
+  @override
+  _PrincipalPageState createState() => _PrincipalPageState();
+}
+
+class _PrincipalPageState extends State<PrincipalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Café Store'),
+        centerTitle: true,
+        backgroundColor: Colors.brown,
+        automaticallyImplyLeading: false,
+        actions: [
+          Column(
+            children: [
+              IconButton(
+                tooltip: 'sair',
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacementNamed(context, 'login');
+                },
+                icon: const Icon(Icons.logout),
+              ),
+              Text(
+                FirebaseAuth.instance.currentUser!.email.toString(),
+                style: const TextStyle(fontSize: 12),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.brown[50],
+      body: Container(
+        padding: const EdgeInsets.all(50),
+      ),
+    );
+  }
+}
